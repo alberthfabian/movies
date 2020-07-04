92c9f668d6b229bc51fc15b0322669576bc082b4
AngelicaSier <alandazabal@sier.com.co>
2020-07-04 07:24:05 -0500

    Change name post to category

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const swaggerUi = require('swagger-ui-express');
 const config = require('../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
-const post = require('./components/post/network');
+const category = require('./components/category/network');
 const movie = require('./components/movie/network');
 const paymethod = require('./components/paymethod/network');
 const errors = require('../network/errors');
@@ -20,7 +20,7 @@ const swaggerDoc = require('./swagger.json');
 // ROUER
 app.use('/api/user', user);
 app.use('/api/auth', auth);
-app.use('/api/post', post);
+app.use('/api/category', category);
 app.use('/api/movie', movie);
 app.use('/api/paymethod', paymethod);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
